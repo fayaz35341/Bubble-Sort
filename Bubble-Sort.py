5216546d688b77dd5f7748a10ef4a95907e87346
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-12-02 20:45:55 +0530

    Optimize bubble sort with early exit condition
    Added an optimization to the bubble sort algorithm to stop early if no swaps were made in an iteration.

--- Bubble-Sort.py
+++ Bubble-Sort.py
@@ -1,9 +1,13 @@
 class Solution:
     def bubbleSort(self, nums):
         for i in range(len(nums)):
+            i_did_swap=0
             for j in range(len(nums)-1):
                 if nums[j]>nums[j+1]:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
+                    i_did_swap=1
+            if i_did_swap==0:
+                break
         return nums
 n=input().strip("[]")
 n=list(map(int,n.split(",")))
